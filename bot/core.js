4887b15d36c4645852e521f69ac4aca698ab36e3
Tcz <nguyenchinh17012k7@gmail.com>
2026-09-21 19:10:51 +0700

    Update core.js

--- bot/core.js
+++ bot/core.js
@@ -140,6 +140,14 @@ function setOwner(chatId) {
 }
 
 
+
+
+console.log("🔎 DB CONFIG:", {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME
+});
 /* =========================
    DATABASE
 ========================= */
